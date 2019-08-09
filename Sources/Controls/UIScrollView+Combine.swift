856d581054b7215590fa5c78373fe26953600868
Joan Disho <dishojoan@gmail.com>
2019-08-09 10:41:35 +0200

    create reachedBottomPublisher(offset:)

--- Sources/Controls/UIScrollView+Combine.swift
+++ Sources/Controls/UIScrollView+Combine.swift
@@ -14,4 +14,22 @@ public extension UIScrollView {
         publisher(for: \.contentOffset)
             .eraseToAnyPublisher()
     }
+
+    /// A publisher emitting if the bottom of the UIScrollView is reached.
+    ///- parameter offset: A threshhold indicating the bottom of the UIScrollView. Default 0.0.
+    ///- returns: AnyPublisher that emits when the bottom of the UIScrollView is reached.
+    func reachedBottomPublisher(offset: CGFloat = 0.0) -> AnyPublisher<Void, Never> {
+        contentOffsetPublisher
+            .map { [weak self] contentOffset -> Bool in
+                guard let self = self else { return false }
+                let visibleHeight = self.frame.height - self.contentInset.top - self.contentInset.bottom
+                let y = contentOffset.y + self.contentInset.top
+                let threshold = max(offset, self.contentSize.height - visibleHeight)
+                return y > threshold
+            }
+            .removeDuplicates()
+            .filter { $0 }
+            .map { _ in () }
+            .eraseToAnyPublisher()
+    }
 }
