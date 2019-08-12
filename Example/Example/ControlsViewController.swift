d522375c69df885fd1daa4bde65dc58805c58aa6
freak4pc <freak4pc@gmail.com>
2019-08-13 00:02:58 +0300

    Update Example & Readme
    This also switches back to CocoaPods for the example projects. SPM is a PITA for local-development.

--- Example/Example/ControlsViewController.swift
+++ Example/Example/ControlsViewController.swift
@@ -18,19 +18,38 @@ class ControlsViewController: UIViewController {
   @IBOutlet private var `switch`: UISwitch!
   @IBOutlet private var datePicker: UIDatePicker!
   @IBOutlet private var console: UITextView!
+  @IBOutlet private var rightBarButtonItem: UIBarButtonItem!
 
   private var subscriptions = Set<AnyCancellable>()
 
   override func viewDidLoad() {
     super.viewDidLoad()
-
+    
+    // Set up some gesture recognizers
+    let leftSwipe = UISwipeGestureRecognizer()
+    leftSwipe.direction = .left
+    view.addGestureRecognizer(leftSwipe)
+    
+    let longPress = UILongPressGestureRecognizer()
+    longPress.minimumPressDuration = 2
+    view.addGestureRecognizer(longPress)
+    
+    let doubleTap = UITapGestureRecognizer()
+    doubleTap.numberOfTapsRequired = 2
+    view.addGestureRecognizer(doubleTap)
+    
+    // Each merge can go up to 8 elements, so we have to chain a few of them ;-)
     Just("Debug Output:")
       .merge(with: segmented.selectedSegmentIndexPublisher.map { "Segmented at index \($0)" },
                    slider.valuePublisher.map { "Slider value is \($0)" },
                    textField.textPublisher.map { "Text Field text is \($0 ?? "")" },
                    button.tapPublisher.map { "Tapped Button" },
                    `switch`.isOnPublisher.map { "Switch is now \($0 ? "On" : "Off")" },
-                   datePicker.datePublisher.map { "Date picker date is \($0)" })
+                   datePicker.datePublisher.map { "Date picker date is \($0)" },
+                   rightBarButtonItem.tapPublisher.map { "Tapped Right Bar Button Item" })
+      .merge(with: leftSwipe.swipePublisher.map { "Swiped Left with Gesture \($0.memoryAddress)" },
+                   longPress.longPressPublisher.map { "Long Pressed with Gesture \($0.memoryAddress)" },
+                   doubleTap.tapPublisher.map { "Double-tapped view with two fingers with Gesture \($0.memoryAddress)" })
       .scan("") { $0 + "\n" + $1 }
       .handleEvents(receiveOutput: { [console] text in
         guard let console = console else { return }
@@ -38,5 +57,11 @@ class ControlsViewController: UIViewController {
       })
       .assign(to: \.text, on: console)
       .store(in: &subscriptions)
+  }
+}
+
+private extension NSObject {
+  var memoryAddress: String {
+    Unmanaged.passUnretained(self).toOpaque().debugDescription
   }
 }
