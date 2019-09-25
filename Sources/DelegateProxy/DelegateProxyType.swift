204be9fdca16bc3d3e1fb89b963572f1b21943d4
Joan Disho <dishojoan@gmail.com>
2019-09-25 21:04:49 +0200

    extend DelegateProxyType to create the proxy for the given object

--- Sources/DelegateProxy/DelegateProxyType.swift
+++ Sources/DelegateProxy/DelegateProxyType.swift
@@ -7,8 +7,29 @@
 
 import Foundation
 
+private var associatedKey: UInt8 = 123
+
 public protocol DelegateProxyType {
     associatedtype Object
 
     func setDelegate(to object: Object)
 }
+
+public extension DelegateProxyType where Self: DelegateProxy {
+
+    static func createDelegateProxy(for object: Object) -> Self {
+        let delegateProxy: Self
+
+        if let associatedObject = objc_getAssociatedObject(object, &associatedKey) as? Self {
+            delegateProxy = associatedObject
+        } else {
+            delegateProxy = .init()
+            objc_setAssociatedObject(object, &associatedKey, delegateProxy, .OBJC_ASSOCIATION_RETAIN)
+        }
+
+        delegateProxy.setDelegate(to: object)
+
+        return delegateProxy
+    }
+}
+
