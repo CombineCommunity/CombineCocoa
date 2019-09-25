f2eb788955a6eef838b10c927831be7e034e5ff7
Joan Disho <dishojoan@gmail.com>
2019-09-25 20:45:26 +0200

    create the interface of DelegateProxy

---
+++ Sources/DelegateProxy/DelegateProxy.swift
@@ -0,0 +1,17 @@
+//
+//  DelegateProxy.swift
+//  CombineCocoa
+//
+//  Created by Joan Disho on 25.09.19.
+//
+
+import Foundation
+
+public final class DelegateProxy {
+
+    private var dict: [Selector: ([Any]) -> Void] = [:]
+
+    public func intercept(_ selector: Selector, handler: @escaping ([Any]) -> Void) {
+        dict[selector] = handler
+    }
+}
