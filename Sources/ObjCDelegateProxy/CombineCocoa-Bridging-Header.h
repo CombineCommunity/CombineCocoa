94d12f2e527e9d78dd8bc0e5d0567f6623d46775
Joan Disho <dishojoan@gmail.com>
2019-09-25 21:25:57 +0200

    create the bridging header

---
+++ Sources/ObjCDelegateProxy/CombineCocoa-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+
+#import <Foundation/Foundation.h>
+#import "ObjCDelegateProxy.h"
+
+FOUNDATION_EXPORT double DelegateProxyVersionNumber;
+FOUNDATION_EXPORT const unsigned char DelegateProxyVersionString[];
