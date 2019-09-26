d6a0de38d79ef1e2e86561929c22ee488ff5fe34
Joan Disho <dishojoan@gmail.com>
2019-09-26 17:17:22 +0200

    create the Combine version of intercept(_:)

---
+++ Sources/DelegateProxy/DelegateProxy+Combine/DelegateProxy+Combine.swift
@@ -0,0 +1,22 @@
+//
+//  DelegateProxy+Combine.swift
+//  CombineCocoa
+//
+//  Created by Joan Disho on 25/09/2019.
+//  Copyright © 2019 Joan Disho. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+public extension DelegateProxy {
+    func interceptPublisher(_ selector: Selector) -> AnyPublisher<[Any], Never> {
+        let publisher = DelegateProxyPublisher<[Any]> { subscriber in
+            return self.intercept(selector) { args in
+                _ = subscriber.receive(args)
+                subscriber.receive(completion: .finished)
+            }
+        }
+        return publisher.eraseToAnyPublisher()
+    }
+}
