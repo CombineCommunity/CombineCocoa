e860fc96118e2a9c23169e8bdfe892ed227ac023
Kevin Renskers <kevin@loopwerk.io>
2020-10-01 14:34:41 +0200

    Needs to be public

--- Sources/CombineCocoa/Controls/UISearchBar+Combine.swift
+++ Sources/CombineCocoa/Controls/UISearchBar+Combine.swift
@@ -12,7 +12,7 @@ import Combine
 
 // swiftlint:disable force_cast
 @available(iOS 13.0, *)
-extension UISearchBar {
+public extension UISearchBar {
     /// Combine wrapper for `UISearchBarDelegate.searchBar(_:textDidChange:)`
     var textDidChangePublisher: AnyPublisher<(searchBar: UISearchBar, searchText: String), Never> {
         let selector = #selector(UISearchBarDelegate.searchBar(_:textDidChange:))
