94c4757d5a5e1cdde9fd108d2b7809b3dba2a86e
Hao Lu <lu.hao@ekohe.com>
2021-02-05 17:09:04 +0800

    Fix UIPageControl currentPagePublisher only emit once bug

--- Sources/CombineCocoa/Controls/UIPageControl+Combine.swift
+++ Sources/CombineCocoa/Controls/UIPageControl+Combine.swift
@@ -14,8 +14,7 @@ import UIKit
 public extension UIPageControl {
     /// A publisher emitting current page changes for this page control.
     var currentPagePublisher: AnyPublisher<Int, Never> {
-        Publishers.ControlProperty(control: self, events: .defaultValueEvents, keyPath: \.currentPage)
-                  .eraseToAnyPublisher()
+        publisher(for: \.currentPage).eraseToAnyPublisher()
     }
 }
 #endif
