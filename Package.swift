584221f3d592e4a336fd5cde4866161c077aa5a7
Yongjun <stareta1202@gmail.com>
2024-03-12 22:55:18 +0900

    Add VisionOS

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "CombineCocoa",
-    platforms: [.iOS(.v10)],
+    platforms: [.iOS(.v10), .visionOS(.v1)],
     products: [
         .library(name: "CombineCocoa", targets: ["CombineCocoa"]),
     ],
